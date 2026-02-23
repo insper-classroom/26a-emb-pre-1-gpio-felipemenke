14ee512c282b1d5b9aca3a511ade031e6adfe4d7
felipemenke <felipelm4@al.insper.edu.br>
2026-02-23 13:09:21 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -18,17 +18,14 @@ int main() {
 
   while (true) {
     gpio_put(LED_PIN_R, 1);
+    gpio_put(LED_PIN_L, 1);    
     sleep_ms(250);
-    
-    gpio_put(LED_PIN_L, 1);
-    sleep_ms(250);
-    
+
     gpio_put(LED_PIN_R, 0);
-    sleep_ms(250);
-
-
-
     gpio_put(LED_PIN_L, 0);
     sleep_ms(250);
+
+
+
   }
 }
