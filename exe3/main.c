a4ad78442022a9d017e41a8d9302e699b222dca6
felipemenke <felipelm4@al.insper.edu.br>
2026-02-23 18:28:37 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -10,26 +10,29 @@ const int LED_PIN_2 = 6;
 int main() {
   stdio_init_all();
 
-  gpio_init(BTN_PIN_1);
-  gpio_set_dir(BTN_PIN_1, GPIO_IN);
-  gpio_pull_up(BTN_PIN_1);
+  gpio_init(BTN_PIN_1); // Inicia o botão 1
+  gpio_set_dir(BTN_PIN_1, GPIO_IN); // entrada e saida do botão 1
+  gpio_pull_up(BTN_PIN_1); // ativa o pull-up para o botão 1, garantindo que ele leia HIGH quando não pressionado
 
-  gpio_init(BTN_PIN_2);
-  gpio_set_dir(BTN_PIN_2, GPIO_IN);
-  gpio_pull_up(BTN_PIN_2);
+  gpio_init(BTN_PIN_2); // Inicia o botão 2
+  gpio_set_dir(BTN_PIN_2, GPIO_IN); // entrada e saida do botão 2
+  gpio_pull_up(BTN_PIN_2); // ativa o pull-up para o botão 2, garantindo que ele leia HIGH quando não pressionado
 
-  gpio_init(LED_PIN_1);
+  gpio_init(LED_PIN_1); 
   gpio_set_dir(LED_PIN_1, GPIO_OUT);
   gpio_put(LED_PIN_1, 0);
 
   gpio_init(LED_PIN_2);
   gpio_set_dir(LED_PIN_2, GPIO_OUT);
   gpio_put(LED_PIN_2, 0);
-
+  
+  bool led1_state = 0;
+  bool led2_state = 0;
+  
   while (true) {
     if (!gpio_get(BTN_PIN_1)) {
-      uint8_t current_state = gpio_get(LED_PIN_1);
-      gpio_put(LED_PIN_1, !current_state);
+      led1_state = !led1_state;
+      gpio_put(LED_PIN_1, led1_state);
       while (!gpio_get(BTN_PIN_1)) {
         sleep_ms(10);
       }
@@ -37,8 +40,8 @@ int main() {
     }
     
     if (!gpio_get(BTN_PIN_2)) {
-      uint8_t current_state = gpio_get(LED_PIN_2);
-      gpio_put(LED_PIN_2, !current_state);    
+      led2_state = !led2_state;
+      gpio_put(LED_PIN_2, led2_state);    
       while (!gpio_get(BTN_PIN_2)) {
         sleep_ms(10);
       }
