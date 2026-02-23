4436a39af8c11199e4a235b104859ac3e66c759d
felipemenke <felipelm4@al.insper.edu.br>
2026-02-23 12:50:28 -0300

    cppcheck4

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 #define NUM_ELEMENTS 5
-
+int a = 5; 
+int b = 5; 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
     return product;
 }
 
 int main(void) {
-    int a = 5; //uninitvar, legacyUninitvar
-    int b = 5; //uninitvar, legacyUninitvar
+    
     int result = multiplyNumbers(a, b);
 
     int arr[result];
