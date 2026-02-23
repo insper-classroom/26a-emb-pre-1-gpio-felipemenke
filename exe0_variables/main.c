342eeca63f9a7d8963eee8d2f2fbacb40da4a1ca
felipemenke <felipelm4@al.insper.edu.br>
2026-02-23 12:52:51 -0300

    variables

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,18 +1,18 @@
 #include "stdio.h"
 
-int a = 0; // var global
-int b = 0; // var global
+int *a = 0; // var global
+int *b = 0; // var global
 
 void foo(void) {
-    a = a + 1; // acessa variavel global
+    *a = *a + 1; // acessa variavel global
 }
 
 void main(void) {
     while (1) {
         foo();
         
-        if (a > 5) {
-            b = 1;
+        if (*a > 5) {
+            *b = 1;
         }
     }
 }
