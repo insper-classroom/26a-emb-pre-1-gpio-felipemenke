de0cc2f19ec5cdc7564f7b1a9acdcef215951a75
felipemenke <felipelm4@al.insper.edu.br>
2026-02-23 23:05:11 -0300

    ex4 - correcao

--- exe4/main.c
+++ exe4/main.c
@@ -2,14 +2,14 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_1 = 5;
+const int LED_PIN_2 = 8;
+const int LED_PIN_3 = 11;
+const int LED_PIN_4 = 15;
+const int BTN_PIN = 28;
+
 int main(void) {
     stdio_init_all();
-
-    const int LED_PIN_1 = 5;
-    const int LED_PIN_2 = 8;
-    const int LED_PIN_3 = 11;
-    const int LED_PIN_4 = 15;
-    const int BTN_PIN = 28;
 
     gpio_init(BTN_PIN);
     gpio_set_dir(BTN_PIN, GPIO_IN);
@@ -33,33 +33,26 @@ int main(void) {
 
     while (true) {
 
-        if (!gpio_get(BTN_PIN)) {
-            sleep_ms(20);  
-            if (!gpio_get(BTN_PIN)) {
-                gpio_put(LED_PIN_1, 1);
-                sleep_ms(300);
+      if (!gpio_get(BTN_PIN)) {
+        gpio_put(LED_PIN_1, 1);
+        sleep_ms(300);
 
-                gpio_put(LED_PIN_1, 0);
-                gpio_put(LED_PIN_2, 1);
-                sleep_ms(300);
+        gpio_put(LED_PIN_1, 0);
+        gpio_put(LED_PIN_2, 1);
+        sleep_ms(300);
 
-                gpio_put(LED_PIN_2, 0);
-                gpio_put(LED_PIN_3, 1);
-                sleep_ms(300);
+        gpio_put(LED_PIN_2, 0);
+        gpio_put(LED_PIN_3, 1);
+        sleep_ms(300);
 
-                gpio_put(LED_PIN_3, 0);
-                gpio_put(LED_PIN_4, 1);
-                sleep_ms(300);
-                gpio_put(LED_PIN_4, 0);
+        gpio_put(LED_PIN_3, 0);
+        gpio_put(LED_PIN_4, 1);
+        sleep_ms(300);
+        gpio_put(LED_PIN_4, 0);
 
-                while (!gpio_get(BTN_PIN)) {
-                    sleep_ms(10);
-                }
-                sleep_ms(10); 
-            }
+        while (!gpio_get(BTN_PIN)) {
         }
-        sleep_ms(5);
+      }
     }
-    return 0;
 }
 
